f9421b732d061348ee7eb4c84dd9868465e14a55
Benjamin D Staples <bestaple@shale.soic.indiana.edu>
2018-12-03 12:21:15 -0500

    guitar controlled menu in guitar_test

--- display/test.c
+++ display/test.c
@@ -1,11 +1,33 @@
 #include <stm32f30x.h>
 #include <f3d_lcd_sd.h>
+#include <f3d_uart.h>
 #include <f3d_delay.h>
+#include <f3d_guitar.h>
+#include <f3d_i2c.h>
 #include <stdio.h>
 
-void main(void) {
+#define BARDOWN(x) ((x.b_one) >> 6)
+#define BARUP(x) ((x.b_two) & 0x1)
+#define MINUS(x) (((x.b_one) >> 4) & 0x1)
+#define PLUS(x) (((x.b_one) >> 2) & 0x1)
+#define BUTTON(x, y) (((x.b_two) >> (3 + y)) & 0x1)) //vals 0->4 y,g,b,r,o
 
-  f3d_lcd_init();
+void selected_item(int x, int y, char* str){
+  f3d_lcd_drawString(x, y, str, RED, BLACK);
+  delay(50);
+  f3d_lcd_drawString(x, y, str, YELLOW, BLACK);
+  delay(50);
+  f3d_lcd_drawString(x, y, str, GREEN, BLACK);
+  delay(50);
+  f3d_lcd_drawString(x, y, str, BLUE, BLACK);
+  delay(50);
+  f3d_lcd_drawString(x, y, str, CYAN, BLACK);
+  delay(50);
+}
+
+void main(void) {
+  int menu_drawn_pause = 0;
+  int menu_item = 0;
 
   f3d_lcd_fillScreen(BLACK);
   f3d_lcd_drawString(55, 40, "DDR", WHITE, BLACK);
@@ -13,6 +35,30 @@ void main(void) {
   f3d_lcd_drawString(30, 60, "Wii Nunchuk", WHITE, BLACK);
   f3d_lcd_drawString(10, 100, "Green Fret to Start", WHITE, BLACK);
   
+  //pause menu
+  while(1) {
+    if(!menu_drawn_pause){
+      f3d_lcd_fillScreen(BLACK);
+      f3d_lcd_drawString(55, 40, "PAUSE", WHITE, BLACK);
+      f3d_lcd_drawString(40, 50, "Restart", WHITE, BLACK);
+      f3d_lcd_drawString(40, 60, "Song Select", WHITE, BLACK);
+      f3d_lcd_drawString(40, 70, "Main Menu", WHITE, BLACK);
+      menu_drawn_pause = 1;
+    }
+    switch(menu_item){
+    case 0:
+      selected_item(40, 50, "Restart");
+      break;
+    case 1:
+      selected_item(40, 60, "Song Select");
+      break;
+    case 2:
+      selected_item(40, 70, "Main Menu");
+      break;
+    }
+    
+    
+  }
   while(1) {
     f3d_lcd_drawString(40, 20, "DDRBWWN", RED, BLACK);
     delay(50);
@@ -25,7 +71,6 @@ void main(void) {
     f3d_lcd_drawString(40, 20, "DDRBWWN", CYAN, BLACK);
     delay(50);
   }
-    
 }
 
 #ifdef USE_FULL_ASSERT
--- guitar_test/main.c
+++ guitar_test/main.c
@@ -44,13 +44,27 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BARDOWN(x) ((x.b_one) >> 6)
+#define BARDOWN(x) (((x.b_one) >> 6) & 0x1)
 #define BARUP(x) ((x.b_two) & 0x1)
 #define MINUS(x) (((x.b_one) >> 4) & 0x1)
 #define PLUS(x) (((x.b_one) >> 2) & 0x1)
 #define BUTTON(x, y) (((x.b_two) >> (3 + y)) & 0x1)) //vals 0->4 y,g,b,r,o
 
 guitar_t gui;
+
+void selected_item(int x, int y, char* str){
+  f3d_lcd_drawString(x, y, str, RED, BLACK);
+  delay(50);
+  f3d_lcd_drawString(x, y, str, YELLOW, BLACK);
+  delay(50);
+  f3d_lcd_drawString(x, y, str, GREEN, BLACK);
+  delay(50);
+  f3d_lcd_drawString(x, y, str, BLUE, BLACK);
+  delay(50);
+  f3d_lcd_drawString(x, y, str, CYAN, BLACK);
+  delay(50);
+}
+
 
 int main(void) { 
   setvbuf(stdin, NULL, _IONBF, 0);
@@ -79,17 +93,48 @@ int main(void) {
   
   int i;
   char button_vals[5] = {'y','g','b','r','o'};
+  int menu_drawn_pause = 0;
+  int menu_item = 0;
+
   while(1){
     f3d_guitar_read(&gui);
+    /*
     for(i=0;i<5;i++){
       printf("BUTTON %c|%d\n",button_vals[i],BUTTON(gui,i);
     }
     printf("\n\n");
     Delay(100);
-    //printf("BARDOWN %d\n",BARDOWN(gui));
-    //printf("BARUP   %d\n",BARUP(gui));
-    //printf("MINUS   %d\n",MINUS(gui));
-    //printf("PLUS    %d\n",PLUS(gui));
+    printf("BARDOWN %d\n",BARDOWN(gui));
+    printf("BARUP   %d\n",BARUP(gui));
+    Delay(1000);
+    printf("MINUS   %d\n",MINUS(gui));
+    printf("PLUS    %d\n",PLUS(gui));
+    */
+    if(!menu_drawn_pause){
+      f3d_lcd_fillScreen(BLACK);
+      f3d_lcd_drawString(55, 40, "PAUSE", WHITE, BLACK);
+      f3d_lcd_drawString(40, 50, "Restart", WHITE, BLACK);
+      f3d_lcd_drawString(40, 60, "Song Select", WHITE, BLACK);
+      f3d_lcd_drawString(40, 70, "Main Menu", WHITE, BLACK);
+      menu_drawn_pause = 1;
+    }
+    switch(menu_item){
+    case 0:
+      selected_item(40, 50, "Restart");
+      break;
+    case 1:
+      selected_item(40, 60, "Song Select");
+      break;
+    case 2:
+      selected_item(40, 70, "Main Menu");
+      break;
+    }
+    if(!BARDOWN(gui)){
+      menu_item = (menu_item + 1) % 3;
+    }
+    if(!BARUP(gui)){
+      menu_item = (menu_item + 2) % 3;
+    }
   }
   
   
