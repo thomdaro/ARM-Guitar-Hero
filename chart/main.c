d24fc7ad1c1e13ae826c1ca362c2f793aa624d35
dgalants <dgalants@iu.edu>
2018-12-06 12:54:29 -0500

    update chart

--- chart/main.c
+++ chart/main.c
@@ -9,9 +9,16 @@
 #include <diskio.h>
 #include "chart.h"
 
+#define BARDOWN(x) (((x.b_one) >> 6) & 0x1)
+#define BARUP(x) ((x.b_two) & 0x1)
+#define MINUS(x) (((x.b_one) >> 4) & 0x1)
+#define PLUS(x) (((x.b_one) >> 2) & 0x1)
+#define BUTTON(x, y) (((x.b_two) >> (3 + y)) & 0x1) //vals 0->4 y,g,b,r,o
+
 FATFS Fatfs;		/* File system object   */
 FIL Fil;			/* File object		  */
 BYTE Buff[128];		/* File read buffer	 */
+guitar_t gui;
 
 void die (FRESULT rc) {
 	printf("Failed with rc=%u.\n", rc);
@@ -62,6 +69,20 @@ void main(void) {
 	event_t 		s_event = {0};
 	single_t 		s_sing = {0};
 
+	uint16_t i = 20;
+
+	printf("\nDirectory listing...\n");
+	for (;;) {
+		rc = f_readdir(&dir, &fno);			// Read a directory item
+		if (rc || !fno.fname[0]) break;	// Error or end of dir
+		f3d_lcd_drawString(40, i, fno.fname, GREEN, BLACK);
+		i += 10;
+	}
+	if (rc) die(rc);
+
+	Delay(10000);
+
+	/*
 	init_song(&Fil, &s_song);
 	init_sync_track(&Fil, &s_sync);
 	init_event(&Fil, &s_event);
@@ -75,6 +96,7 @@ void main(void) {
   f3d_lcd_drawString(10, 80, s_song.genre, WHITE, BLACK);
   f3d_lcd_drawString(10, 90, "PATH:", GREEN, BLACK);
   f3d_lcd_drawString(10, 100, s_song.music_stream, WHITE, BLACK);
+	*/
 
 	f_close(&Fil); // NOTE: MAKE SURE YOU DON'T CALL IT RIGHT AFTER INITs. WILL BREAK THE CARD
 
